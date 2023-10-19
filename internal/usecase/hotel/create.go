0070decf637885aaf47a76e760fe9877f29dedce
Zaman <okaymans44@gmail.com>
2023-10-19 09:12:28 +0300

    upd: usecase/hotel/create.go number regexp validation

--- internal/usecase/hotel/create.go
+++ internal/usecase/hotel/create.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	validation "github.com/go-ozzo/ozzo-validation"
 	"github.com/niumandzi/nto2022/model"
+	"regexp"
 )
 
 func (h HotelUsecase) CreateHotel(ctx context.Context, hotel model.Hotel) (int, error) {
@@ -14,7 +15,7 @@ func (h HotelUsecase) CreateHotel(ctx context.Context, hotel model.Hotel) (int, 
 	err := validation.ValidateStruct(&hotel,
 		validation.Field(&hotel.Name, validation.Required, validation.Length(0, 200)),
 		validation.Field(&hotel.LocationId, validation.Required),
-		validation.Field(&hotel.Number, validation.Required),
+		validation.Field(&hotel.Number, validation.Required, validation.Match(regexp.MustCompile(`^\+\d{1,2}\s\(\d{3}\)\s\d{3}-\d{2}-\d{2}$`))),
 		validation.Field(&hotel.WorkerId, validation.Required),
 		validation.Field(&hotel.Description, validation.Required, validation.Length(0, 500)),
 	)
