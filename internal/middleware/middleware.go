9b38f7ea5758a31bb1de21efe04f2ce97bf4e4ca
Zaman <okaymans44@gmail.com>
2023-10-27 19:35:12 +0300

    add: gin

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -4,8 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/niumandzi/nto2022/internal/middleware/handler"
 	"github.com/niumandzi/nto2022/internal/usecase"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func NewClient(cases *usecase.UseCases) *gin.Engine {
@@ -14,7 +12,6 @@ func NewClient(cases *usecase.UseCases) *gin.Engine {
 	h := handler.NewHandler(cases)
 
 	r.POST("/create_hotel", h.CreateHotel)
-	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	return r
 }
